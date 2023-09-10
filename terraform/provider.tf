d24aafe2f1f3dd91597008592c3d03a4e78212aa
alexandra_vishneva123 <vishneva123123@gmail.com>
2023-09-10 19:30:38 -0400

    jenkins & terraform conf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~>4.0"
-    }
-  }
-}
-
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
